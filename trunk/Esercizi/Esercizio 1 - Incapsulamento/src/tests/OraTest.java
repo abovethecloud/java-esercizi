e38a87c705797829be5bec6834867c72c9964d0c
claudio.peroni93@gmail.com <claudio.peroni93@gmail.com@22d09cae-5ee1-0056-7aee-a976fd968a71>
2014-12-28 15:44:19 +0000

    Ipotesi di Test

--- trunk/Esercizi/Esercizio 1 - Incapsulamento/src/tests/OraTest.java
+++ trunk/Esercizi/Esercizio 1 - Incapsulamento/src/tests/OraTest.java
@@ -1,6 +1,18 @@
 package tests;
 
+import time.Ora;
+
 public class OraTest {
+	
+	public static void main(String[] args) {
+		
+		Ora ora1 = new Ora(25, 31);
+		Ora ora2 = new Ora(12, 20);
+		Ora ora3 = new Ora(-5, 62);
+		
+		System.out.println(ora2.getDiffMinuti(ora3));
+		
+	}
 	
 	
 	
