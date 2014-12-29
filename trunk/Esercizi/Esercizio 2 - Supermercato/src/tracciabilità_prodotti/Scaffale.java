0995ae563a50a407624b46eaec942e9f506ca535
claudio.peroni93@gmail.com <claudio.peroni93@gmail.com@22d09cae-5ee1-0056-7aee-a976fd968a71>
2014-12-29 14:34:48 +0000

    Aggiunto allo Scaffale i set, i get, il costruttore. TODO: Creare metodo che restituisce il prodotto dello scaffale a partire dal codice

--- trunk/Esercizi/Esercizio 2 - Supermercato/src/tracciabilità_prodotti/Scaffale.java
+++ trunk/Esercizi/Esercizio 2 - Supermercato/src/tracciabilità_prodotti/Scaffale.java
@@ -2,4 +2,43 @@ package tracciabilità_prodotti;
 
 public class Scaffale {
 
+	private int capienza;
+	private ProdottoDelSupermercato[] prodottiScaffale;
+
+	public int getCapienza() {
+		return capienza;
+	}
+
+	public void setCapienza(int capienza) {
+		if (capienza < 1)
+			capienza = 1;
+		if (capienza < this.capienza) // Per semplicità gli scaffali si possono
+										// allargare ma non restringere
+			capienza = this.capienza;
+		this.capienza = capienza;
+	}
+
+	public ProdottoDelSupermercato[] getProdottiScaffale() {
+		return prodottiScaffale;
+	}
+
+	public void setProdottiScaffale(ProdottoDelSupermercato[] prodottiScaffale) {
+		if (prodottiScaffale.length <= getCapienza())
+			this.prodottiScaffale = prodottiScaffale;
+		else
+			System.out.println("Errore: Troppi prodotti per lo scaffale");
+	}
+
+	public Scaffale(int capienza, ProdottoDelSupermercato... prodotti) {
+		super();
+		setCapienza(capienza);
+		setProdottiScaffale(prodotti);
+
+	}
+	
+	public ProdottoDelSupermercato prodottoDaCodice(String cod) {
+		// TODO: Restituire il prodotto a partire dal codice
+		return new ProdottoDelSupermercato("NULL",0);
+	}
+
 }
