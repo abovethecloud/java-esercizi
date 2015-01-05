c635337715abcaca8747614075ee18c893a658fa
claudio.peroni93@gmail.com <claudio.peroni93@gmail.com@22d09cae-5ee1-0056-7aee-a976fd968a71>
2015-01-05 14:57:34 +0000

    Aggiunte minori

--- Esercizi/Esercizio 5 - Drawings/src/drawings/ColoredDrawable.java
+++ Esercizi/Esercizio 5 - Drawings/src/drawings/ColoredDrawable.java
@@ -23,6 +23,12 @@ public class ColoredDrawable implements Drawable {
 		this.drawable = drawable;
 	}
 
+	/**
+	 * Questo metodo Draw disegna il drawable secondo le sue particolari
+	 * istruzioni, settando però prima il colore al colore scelto nel
+	 * ColoredDrawable. Dopo il disegno si cura anche di ripristinare il colore
+	 * di default con la chiamata ai DefaultSettings.
+	 */
 	@Override
 	public void draw(Graphics graphics) {
 		graphics.setColor(color);
--- Esercizi/Esercizio 5 - Drawings/src/drawings/DefaultSettings.java
+++ Esercizi/Esercizio 5 - Drawings/src/drawings/DefaultSettings.java
@@ -10,36 +10,41 @@ import java.awt.Color;
  * @see StrokedDrawable
  */
 public class DefaultSettings {
-	
-	private static DefaultSettings settings=
-		new DefaultSettings();
 
-	private DefaultSettings(){
-		
+	private static DefaultSettings settings = new DefaultSettings();
+
+	/**
+	 * Costruttore di default. Does nothing.
+	 */
+	private DefaultSettings() {
 	}
-	
+
+	/**
+	 * Metodo che permette l'accesso ai settings contenuti in DefaultSettings.
+	 * 
+	 * @return settings = new DefaultSettings()
+	 */
 	public static DefaultSettings getSettings() {
 		return settings;
 	}
-	
-	private int width=1;
-	private Color color=Color.BLACK;
-	
+
+	private int width = 1;
+	private Color color = Color.BLACK;
+
 	public int getWidth() {
 		return width;
 	}
-	
+
 	public void setWidth(int width) {
 		this.width = width;
 	}
-	
+
 	public Color getColor() {
 		return color;
 	}
-	
+
 	public void setColor(Color color) {
 		this.color = color;
 	}
-	
-	
+
 }
