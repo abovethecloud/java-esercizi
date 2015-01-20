29942d89cdd7bbf6f49d4fabbfc0cbdde49223b7
claudio.peroni93@gmail.com <claudio.peroni93@gmail.com@22d09cae-5ee1-0056-7aee-a976fd968a71>
2015-01-20 22:57:37 +0000

    Aggiunto qualche commento soprattutto riguardante il metodo paintComponent (di cui viene fatto l'override in DrawingsPanel) e il frame.setVisible (visto ad esempio in CompositeTest) che, apparentemente, triggera il suddetto metodo mentre rende visibile il frame, difatto permettendo la visualizzazione dei disegni invece di un pannello vuoto.

--- trunk/Esercizi/Esercizio 5 - Drawings/src/drawings/DrawingsPanel.java
+++ trunk/Esercizi/Esercizio 5 - Drawings/src/drawings/DrawingsPanel.java
@@ -5,6 +5,12 @@ import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
+/**
+ * Questa classe estende JPanel e vi aggiunge qualche funzione utile al disegno: un ArrayList di Drawables e un override di paintComponent
+ * 
+ * @author claudio
+ *
+ */
 public class DrawingsPanel extends JPanel{
 
 	private ArrayList<Drawable> drawables=
@@ -14,6 +20,11 @@ public class DrawingsPanel extends JPanel{
 		drawables.add(drawable);
 	}
 	
+	/**
+	 * Questo metodo fa l'override (BUONO) di paintComponent di JPanel.
+	 * Viene chiamato in automatico quando nel frame di appartenenza viene settata a true la visibilità.
+	 * In questo modo NON è necessario passare esplicitamente un elemento grafico di tipo Graphics.
+	 */
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
--- trunk/Esercizi/Esercizio 5 - Drawings/src/tests/CompositeTest.java
+++ trunk/Esercizi/Esercizio 5 - Drawings/src/tests/CompositeTest.java
@@ -1,31 +1,63 @@
 package tests;
 
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
 
 import drawings.Circle;
 import drawings.CompositeDrawable;
 import drawings.DrawingsPanel;
 
+/**
+ * Test che verifica la funzionalità di CommpositeDrawable aggiungendo elementi
+ * ad un CompositeDrawable e disegnando.
+ * 
+ * @author claudio
+ *
+ */
 public class CompositeTest {
-	
-	public static void main(String[] args) {
-		
-		CompositeDrawable comp = new CompositeDrawable();
-		
-		comp.getDrawables().add(new Circle(100, 100, 100, true));
-		comp.getDrawables().add(new Circle(200, 100, 10, false));
 
-		
+	public static void main(String[] args) {
+
+		// Istanzio un CompositeDrawable
+		CompositeDrawable comp = new CompositeDrawable();
+
+		// Aggiungo 2 cerchi (uno vuoto e uno colorato)
+		comp.getDrawables().add(new Circle(100, 100, 100, false));
+		comp.getDrawables().add(new Circle(200, 100, 10, true));
+
+		/*
+		 * Istanzio un pannello di tipo DrawingsPanel (ovvero apposta per il
+		 * disegno) e aggiungo il CompositeDrawable. La particolarità di
+		 * DrawingsPanel è quella di possedere il metodo addDrawable, che
+		 * aggiunge il Drawable ad un ArrayList contenuto nel pannello. Lo
+		 * disegno tutto chiamando paintComponent
+		 */
 		DrawingsPanel panel = new DrawingsPanel();
 		panel.addDrawable(comp);
-		
+
+		// Creo un frame e aggiungo il pannello
 		JFrame frame = new JFrame("PROVA");
 		frame.setSize(500, 700);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		frame.getContentPane().add(panel);
+
+		// Aggiungo il pannello al frame e setto la visibilità
 		frame.setVisible(true);
+
+		frame.getContentPane().add(panel);
+		frame.repaint();
 		
+
+		/* ATTENZIONE: setVisible !!!
+		 * Il comando setVisible non solo rende visibile il pannello, ma disegna
+		 * anche tutte le componenti chiamando il metodo paintComponent del
+		 * pannello. Morale: Quando costruisco un pannello personalizzato per
+		 * mezzo dell'ereditarietà applicata a JPanel, devo fare l'override
+		 * BUONO (quello con super.paintComponent) del metodo paintComponent,
+		 * dando le istruzioni per il disegno delle componenti del pannello.
+		 * 
+		 */
+
 	}
 
 }
