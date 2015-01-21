b44cb9c34679f7d6ea39cc1550d05ddf0e445f61
claudio.peroni93@gmail.com <claudio.peroni93@gmail.com@22d09cae-5ee1-0056-7aee-a976fd968a71>
2015-01-21 10:35:27 +0000

    Aggiunti principalmente i commenti sulle curve (Curve, CurveDrawable..) Guardando a DropModel e al Test0007, che disegna delle curve di prova.

--- trunk/Esercizi/Esercizio 5 - Drawings/src/drawings/Curve.java
+++ trunk/Esercizi/Esercizio 5 - Drawings/src/drawings/Curve.java
@@ -1,12 +1,49 @@
 package drawings;
 
-public interface Curve {
-	
-	public float getMinT();
+import java.nio.channels.SeekableByteChannel;
 
+/**
+ * Interfaccia della generica curva. "t" rappresenta il parametro della curva
+ * (parametrizzata come in Analisi 2, con t che varia in un intervallo, come da
+ * 0 a 1. "X" e "Y" sono le coordinate di un punto della curva, e vengono
+ * determinate sostituendo "t" all'interno della parametrizzazione della curva.
+ * 
+ * @author Claudio
+ * 
+ * @see CurveDrawable
+ * @see DropModel
+ * @see FlowerModel
+ * @see SpiralModel
+ *
+ */
+
+public interface Curve {
+
+	/**
+	 * Ogni curva può essere parametrizzata in un intervallo diverso.
+	 * 
+	 * @return minT: il minimo dell'intervallo di parametrizzazione in cui
+	 *         spazia t
+	 */
+	public float getMinT();
+	
+	/**
+	 * Ogni curva può essere parametrizzata in un intervallo diverso.
+	 * 
+	 * @return maxT: il massimo dell'intervallo di parametrizzazione in cui
+	 *         spazia t
+	 */
 	public float getMaxT();
-	
+
+	/**
+	 * @param t indica il parametro in base a cui calcolare l'ascissa
+	 * @return X: l'ascissa del punto
+	 */
 	public float getX(float t);
-	
+
+	/**
+	 * @param t indica il parametro in base a cui calcolare l'ordinata
+	 * @return Y: l'ordinata del punto
+	 */
 	public float getY(float t);
 }
--- trunk/Esercizi/Esercizio 5 - Drawings/src/drawings/CurveDrawable.java
+++ trunk/Esercizi/Esercizio 5 - Drawings/src/drawings/CurveDrawable.java
@@ -2,29 +2,60 @@ package drawings;
 
 import java.awt.Graphics;
 
-public class CurveDrawable implements Drawable{
+/**
+ * La generica curva (interfaccia Curve) non dispone di un metodo di disegno
+ * DRAW. Questa classe incapsula un'altra curva e, come un decorator, supplisce
+ * alla mancanza fornendo un metodo di disegno draw.
+ * 
+ * @author claudio
+ *
+ */
+public class CurveDrawable implements Drawable {
 
 	private Curve curve;
+
 	public CurveDrawable(Curve curve) {
 		super();
 		this.curve = curve;
 	}
+
+	/**
+	 * Questo metodo draw disegna la generica curva tracciando dei piccoli
+	 * segmenti (dimensione determinata da deltaT, a sua volta determinato da
+	 * Nsegmenti) dal punto di partenza al punto di arrivo, passando per tutti i
+	 * punti nel mezzo. I punti sono determinati mediante le regole della curva
+	 * specifica (vengono dunque chiamati i metodi getX(t) e getY(t), dove t è
+	 * il parametro, che progredisce da un minimo ad un massimo (ad esempio da 0
+	 * a 1 a seconda dei metodi getMinT() e getMaxT() della curva) a passi di
+	 * deltaT. Ogni segmento viene tracciato dal punto determinato mediante t1 e
+	 * dal punto determinato mediante t1+deltaT.
+	 * Il seguente t1 non è il t2 precedente, ma viene determinato in base al
+	 * passo del ciclo for a cui mi trovo, mediante deltaT*i.
+	 * 
+	 * Tl;Dr: determino due punti della curva inserendo i parametri (ad es. compresi tra 0 e 1)
+	 * nell'equazione caratteristica della curva, e traccio un segmento che li congiunga.
+	 * In pratica approssimo la curva a segmenti. Piu' segmenti, piu' precisione.
+	 * 
+	 * @param
+	 */
 	@Override
 	public void draw(Graphics graphics) {
-		
-		float min=curve.getMinT();
-		float max=curve.getMaxT();
-		int Nsegmenti=100;
-		float deltaT=(max-min)/Nsegmenti;
-		
+
+		// Determino il deltaT
+		float min = curve.getMinT();
+		float max = curve.getMaxT();
+		int Nsegmenti = 1000;	// era 100
+		float deltaT = (max - min) / Nsegmenti;
+
+		// Disegno tutti i segmenti in un ciclo for
 		for (int i = 0; i < Nsegmenti; i++) {
-			float t1=deltaT*i;
-			float t2=t1+deltaT;
-			graphics.drawLine((int)curve.getX(t1), 
-					(int)curve.getY(t1), 
-					(int)curve.getX(t2), 
-					(int)curve.getY(t2));
+			float t1 = deltaT * i;
+			float t2 = t1 + deltaT;
+			
+			// traccio una retta dal punto P1(getX(t1),getY(t2)) a P2.
+			graphics.drawLine((int) curve.getX(t1), (int) curve.getY(t1),
+					(int) curve.getX(t2), (int) curve.getY(t2));
 		}
-		
+
 	}
 }
