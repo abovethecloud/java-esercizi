c546450dc2fdeeb5f45dd6806f284641a4374cc0
claudio.peroni93@gmail.com <claudio.peroni93@gmail.com@22d09cae-5ee1-0056-7aee-a976fd968a71>
2015-01-21 11:27:08 +0000

    Aggiunti commenti sullo StrokedDrawable, che permette di colorare il bordo della forma. Creato un semplice test con qualche verifica al riguardo: FilledShapeStrokedTest

--- Esercizi/Esercizio 5 - Drawings/src/drawings/StrokedDrawable.java
+++ Esercizi/Esercizio 5 - Drawings/src/drawings/StrokedDrawable.java
@@ -4,24 +4,48 @@ import java.awt.BasicStroke;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
-public class StrokedDrawable implements Drawable{
+import tests.Test0004;
+
+/**
+ * Classe Decorator di un qualunque Drawable (può anche essere un
+ * CompositeDrawable o ColoredDrawable). Aggiunge l'effetto pennello di disegno
+ * dei bordi, con un determinato diametro dato dal parametro width.
+ * 
+ * @author Claudio
+ * 
+ * @see Drawable
+ * @see Test0004
+ *
+ */
+public class StrokedDrawable implements Drawable {
 
 	private Drawable drawable;
 	private int width;
-	
+
 	public StrokedDrawable(Drawable drawable, int width) {
 		super();
 		this.drawable = drawable;
 		this.width = width;
 	}
-	
+
+	/**
+	 * Cambia le impostazioni di "graphics", trasformandolo in tipo Graphics2D e
+	 * aggiungendo l'effetto pennello (che resterà memorizzato in graphics e
+	 * diverrà visibile se presente al momento della chiamata di draw
+	 * (generalmente dovuta ad un paintComponent)
+	 */
 	@Override
 	public void draw(Graphics graphics) {
-		Graphics2D graphics2D=(Graphics2D)graphics;
+
+		/*
+		 * Casting di "graphics" dal tipo Graphics al tipo Graphics2D (che
+		 * estende Graphics), che permette di utilizzare funzioni come il
+		 * pennello.
+		 */
+		Graphics2D graphics2D = (Graphics2D) graphics;
 		graphics2D.setStroke(new BasicStroke(width));
 		drawable.draw(graphics);
-		graphics2D.setStroke(new BasicStroke(
-				DefaultSettings.
-				getSettings().getWidth()));
+		graphics2D.setStroke(new BasicStroke(DefaultSettings.getSettings()
+				.getWidth()));
 	}
 }
