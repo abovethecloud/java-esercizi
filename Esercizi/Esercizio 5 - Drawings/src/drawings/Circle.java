a4b3529bfffc1f65c99e97622e0f1a82c0b3e9b6
claudio.peroni93@gmail.com <claudio.peroni93@gmail.com@22d09cae-5ee1-0056-7aee-a976fd968a71>
2015-01-21 10:57:00 +0000

    Aggiunti commenti riguardanti le Shape (e anche a Circle, che estende Shape)

--- Esercizi/Esercizio 5 - Drawings/src/drawings/Circle.java
+++ Esercizi/Esercizio 5 - Drawings/src/drawings/Circle.java
@@ -3,16 +3,28 @@ package drawings;
 import java.awt.Graphics;
 
 /**
- * Un cerchio dato il centro (x,y) e il raggio
- * radius
+ * Un cerchio dato il centro (x,y), il raggio radius e (eventualmente) il valore
+ * di filled, che rappresenta se il cerchio è pieno o vuoto
  * 
- * @author Alessandro
+ * @author Claudio
  */
-public class Circle extends Shape implements Drawable{
+public class Circle extends Shape implements Drawable {
 
-	private int x,y;
+	private int x, y;
 	private int radius;
-	
+
+	/**
+	 * Costruttore che disegna il cerchio dati SOLO centro e raggio. In
+	 * automatico setta filled a false chiamando il costruttore della classe
+	 * padre Shape [Shape(false) -> filled = false] con super(false).
+	 * 
+	 * @param x
+	 *            ascissa centro cerchio
+	 * @param y
+	 *            ordinata centro cerchio
+	 * @param radius
+	 *            raggio cerchio
+	 */
 	public Circle(int x, int y, int radius) {
 		super(false);
 		this.x = x;
@@ -20,7 +32,22 @@ public class Circle extends Shape implements Drawable{
 		this.radius = radius;
 	}
 
-	public Circle(int x, int y, int radius,boolean filled) {
+	/**
+	 * Costruttore che disegna il cerchio dati centro, raggio e "filled".
+	 * "filled" indica se il cerchio sarà pieno o vuoto. Setta filled al valore
+	 * booleano passato al costruttore chiamando il costruttore della classe
+	 * padre Shape [Shape(...) -> filled = ...] con super(...).
+	 * 
+	 * @param x
+	 *            ascissa centro cerchio
+	 * @param y
+	 *            ordinata centro cerchio
+	 * @param radius
+	 *            raggio cerchio
+	 * @param filled
+	 *            valore booleano che determina il riempimento del cerchio
+	 */
+	public Circle(int x, int y, int radius, boolean filled) {
 		super(filled);
 		this.x = x;
 		this.y = y;
@@ -30,33 +57,37 @@ public class Circle extends Shape implements Drawable{
 	public int getX() {
 		return x;
 	}
-	
+
 	public void setX(int x) {
 		this.x = x;
 	}
-	
+
 	public int getY() {
 		return y;
 	}
+
 	public void setY(int y) {
 		this.y = y;
 	}
-	
+
 	public int getRadius() {
 		return radius;
 	}
-	
+
 	public void setRadius(int radius) {
 		this.radius = radius;
 	}
-	
-	public void draw(Graphics graphics){
-		if(isFilled()){
-			graphics.fillOval(x-radius, y-radius, 
-					radius<<1, radius<<1);
-		}else{
-			graphics.drawOval(x-radius, y-radius, 
-					radius<<1, radius<<1);
+
+	/**
+	 * Disegna un cerchio pieno o vuoto a seconda del valore di super.filled.
+	 * Per conoscere il vlaore booleano di super.filled chiamo il metodo della
+	 * classe Padre isFilled.
+	 */
+	public void draw(Graphics graphics) {
+		if (isFilled()) {
+			graphics.fillOval(x - radius, y - radius, radius << 1, radius << 1);
+		} else {
+			graphics.drawOval(x - radius, y - radius, radius << 1, radius << 1);
 		}
 	}
 }
--- Esercizi/Esercizio 5 - Drawings/src/drawings/Shape.java
+++ Esercizi/Esercizio 5 - Drawings/src/drawings/Shape.java
@@ -1,12 +1,16 @@
 package drawings;
 
+import java.awt.Graphics;
 
 /**
- * TODO
+ * Classe astratta che implementa Drawable e vi aggiunge una funzionalità
+ * (assomiglia ad un Decorator), ovvero il parametro filled, che può determinare
+ * se la Shape dovrà essere piena o vuota (di default vengono disegnate figure
+ * vuote)
  * 
- * @author Alessandro
+ * @author Claudio
  */
-public abstract class Shape implements Drawable{
+public abstract class Shape implements Drawable {
 
 	private boolean filled;
 
@@ -14,14 +18,22 @@ public abstract class Shape implements Drawable{
 		super();
 		this.filled = filled;
 	}
-	
+
 	public void setFilled(boolean filled) {
 		this.filled = filled;
 	}
-	
+
 	public boolean isFilled() {
 		return filled;
 	}
-	
-	//public abstract void draw(Graphics graphics);
+
+	/**
+	 * Metodo astratto draw di disegno. In realtà non è necessario definire così
+	 * draw. Essendo una classe astratta che implementa l'interfaccia, avrei
+	 * anche potuto non scrivere nulla e lasciare sottinteso che si trattase di
+	 * un metodo da implementare in qualunque classe estenda la classe astratta.
+	 * 
+	 */
+	@Override
+	public abstract void draw(Graphics graphics);
 }
