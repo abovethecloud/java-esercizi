4ac94745453967521183af9c29b106b72e2f2659
claudio.peroni93@gmail.com <claudio.peroni93@gmail.com@22d09cae-5ee1-0056-7aee-a976fd968a71>
2015-01-25 23:04:34 +0000

    Aggiunto al Test0011 un fiore (e di conseguenza aggiunto a drawings.generators un FlowerGenerator

--- trunk/Esercizi/Esercizio 5 - Drawings/src/tests/Test0011.java
+++ trunk/Esercizi/Esercizio 5 - Drawings/src/tests/Test0011.java
@@ -10,6 +10,7 @@ import drawings.Drawable;
 import drawings.DrawingsPanel;
 import drawings.builders.BuilderDrawable1;
 import drawings.generators.CircleGenerator;
+import drawings.generators.FlowerGenerator;
 
 public class Test0011 {
 	
@@ -20,17 +21,20 @@ public class Test0011 {
 		
 		// Aggiungo un generatore e qualche colore
 		director.addGenerator("Cerchio", new CircleGenerator());
+		director.addGenerator("Fiore", new FlowerGenerator());
 		director.addColor("Rosso", Color.RED);
 		director.addColor("Verde", Color.GREEN);
 		director.addColor("Giallo", Color.YELLOW);
 		director.addColor("Blu", Color.BLUE);
 		
 		// Passo la stringa al Direttore che la farà assemblare in base al builder e al generator appropriato
-		Drawable draw = director.build("Cerchio Rosso 100 100 50");
+		Drawable draw = director.build("Cerchio Blu 122 122 100");
+		Drawable draw1 = director.build("Fiore Giallo 122 122 3 123");
 		
 		// Creo pannello e aggiungo il drawable
 		DrawingsPanel panel = new DrawingsPanel();
 		panel.addDrawable(draw);
+		panel.addDrawable(draw1);
 		
 		// Creo il frame, aggiusto i settings e vi aggiungo il panel
 		JFrame frame = new JFrame("Test0011");
