5253bb138e8dd48b95a496e5f21d937f9bf501ae
claudio.peroni93@gmail.com <claudio.peroni93@gmail.com@22d09cae-5ee1-0056-7aee-a976fd968a71>
2015-02-09 19:19:38 +0000

    Iniziato il tema

---
+++ trunk/Temi d'esame/Esame 1 - Centro Commerciale/src/centroCommerciale/Locale.java
@@ -0,0 +1,21 @@
+package centroCommerciale;
+
+public class Locale {
+	
+	private int identificativoLocale;
+	private int metraturaLocale;
+	
+	public Locale(int identificativo, int metratura) {
+		this.identificativoLocale = identificativo;
+		this.metraturaLocale = metratura;
+	}
+	
+	public int getIdentificativoLocale() {
+		return identificativoLocale;
+	}
+
+	public int getMetraturaLocale() {
+		return metraturaLocale;
+	}
+
+}
