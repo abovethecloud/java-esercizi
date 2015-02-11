08c43c8add451fd94e7af9dfdd789e02e9c943a0
claudio.peroni93@gmail.com <claudio.peroni93@gmail.com@22d09cae-5ee1-0056-7aee-a976fd968a71>
2015-02-11 22:00:37 +0000

    Aggiunti commenti sugli esempi di interfacce grafiche

--- Laboratorio/Progetti/Progetto04/src/ui/Example04.java
+++ Laboratorio/Progetti/Progetto04/src/ui/Example04.java
@@ -17,8 +17,10 @@ public class Example04 {
 
 	public static void main(String[] args) {
 		
+		/* Istanzio il frame di tipo ExampleUtils */
 		JFrame frame = ExamplesUtils.createFrame();
 		
+		/* Creo la MnuBar, il menu e vi aggiungo i MenuItem */
 		JMenuBar menubar = new JMenuBar();
 			JMenu menu = new JMenu("Ordinamenti");
 			menubar.add(menu);
@@ -30,26 +32,30 @@ public class Example04 {
 			menu.add(menuItem03);
 		frame.setJMenuBar(menubar);
 		
+		/* Aggiungo il SUPERpannello con Layout al frame (conterrà 2 pannelli) */
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(2,1));
 		frame.getContentPane().add(panel);
 		
+		/* Il pannel1 contiene a sua volta 2 componenti in un GridLayout: un JTextField e un JButton */
 		JPanel panel1 = new JPanel();
 			panel1.setLayout(new GridLayout(2, 1));
 			panel.add(panel1);
 					
 			JTextField field = new JTextField("Scrivi qui");
 			panel1.add(field);
+			
 			JButton button = new JButton ("Aggiungi");
 			panel1.add(button);
 			
+		/* Il pannel2 contiene 1 solo componente in un GridLayout(1,1): una JTextArea */
 		JPanel panel2 = new JPanel();
 			panel2.setLayout(new GridLayout(1,1));
 			panel.add(panel2);
 			
-		JTextArea area = new JTextArea("elenco..");
-		area.setEnabled(false);
-		panel2.add(area);
+			JTextArea area = new JTextArea("elenco..");
+			area.setEnabled(false);
+			panel2.add(area);
 		
 		frame.setVisible(true);
 		
