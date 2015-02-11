735f2e46d0dcca2c134bab7218d62a234e3830ff
claudio.peroni93@gmail.com <claudio.peroni93@gmail.com@22d09cae-5ee1-0056-7aee-a976fd968a71>
2015-02-11 22:00:37 +0000

    Aggiunti commenti sugli esempi di interfacce grafiche

--- trunk/Laboratorio/Progetti/Progetto04/src/ui/Example03.java
+++ trunk/Laboratorio/Progetti/Progetto04/src/ui/Example03.java
@@ -16,31 +16,34 @@ public class Example03 {
 	public static void main(String[] args) {
 		
 		JFrame frame = ExamplesUtils.createFrame();
-		
+
 		// commento perché il BorderLayout è di default.
-//		frame.getContentPane().setLayout(new BorderLayout());
+		// frame.getContentPane().setLayout(new BorderLayout());
 		
 		JPanel panel = new JPanel();
 		frame.getContentPane().add(panel);
 		
 		panel.setLayout(new GridLayout(3, 1));
 		
+		/* Creo e aggiungo una semplice e comune casella di testo */
 		JTextArea area = new JTextArea();
 		panel.add(area);
 		
+		/* Questa casella di testo NON E' EDITABILE ed è di color CYAN. Di base ha un testo. Viene anche aggiunto un bordo! */
 		JTextArea area2 = new JTextArea();
-		area2.setBackground(Color.CYAN);
-		area2.setEditable(false);
-		area2.setText("NANAN");
+		area2.setBackground(Color.CYAN);	// COLORE
+		area2.setEditable(false);	// NON EDITABILE
+		area2.setText("Area di testo non editabile");	//TESTO BASE
 		panel.add(area2);
 		
-		area2.setBorder(BorderFactory.createEtchedBorder());
+		area2.setBorder(BorderFactory.createEtchedBorder(Color.BLUE, Color.GREEN));	// BORDO alla casella di testo!
 
+		/* Area di testo con ScrollPane (DECORATOR) */
 		JTextArea area3 = new JTextArea();
 		area3.setBackground(Color.YELLOW);
-		area3.setText("ffffffjhbuhcfyde");
+		area3.setText("Area di testo con ScrollPane");
 		panel.add(area3);
-		JScrollPane scrollPane = new JScrollPane(area3);
+		JScrollPane scrollPane = new JScrollPane(area3);	// ScrollPane è un DECORATOR su JTextArea come su ogni altro componente!
 		panel.add(scrollPane);
 		
 		frame.setVisible(true);
