99dae0069eb2a98219cedd91a5e443cca9964225
abovethecloud <claudio.peroni93@gmail.com>
2015-04-21 22:24:38 +0200

    Minor changes. Missing the Runnable implementation! It was probably forgotten on the B2 account.

--- Laboratorio/HttpServer/src/server/Server.java
+++ Laboratorio/HttpServer/src/server/Server.java
@@ -1,14 +1,10 @@
 package server;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
-import java.util.LinkedList;
 
 public class Server {
 
