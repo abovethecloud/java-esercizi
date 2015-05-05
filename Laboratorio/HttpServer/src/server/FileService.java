9dd862c125d50d18df1ce5ac2dd0cf55bde53771
abovethecloud <claudio.peroni93@gmail.com>
2015-05-05 23:03:19 +0200

    HttpServer: enhancements to FileService.
    If the http request asks for a file, the IService to be used is
    FileService. Now FileService is able to handle .xml files.

--- Laboratorio/HttpServer/src/server/FileService.java
+++ Laboratorio/HttpServer/src/server/FileService.java
@@ -8,32 +8,61 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 
+import server.HttpMessage.ContentType;
+
 public class FileService implements IService {
 
+	/**
+	 * This method checks if the URI is valid and then answers the HTTP request,
+	 * accordingly to the type of content.
+	 */
 	public void sendHTTP(final Socket clientSocket, HttpRequest request)
 			throws IOException, FileNotFoundException {
-		
+
 		String filename = checkURI(request.getUri());
 		HttpMessage message = new HttpMessage();
+
+		// TODO: This clearly isn't a versatile way to handle the various
+		// formats. Must generalize for many (interface..?)
+		if (filename.endsWith("xml")) {
+			message.setContentType(ContentType.XML);
+		}
+
+		// Answers the HTTP request sending the requested file
 		message.openHttpAnswer(clientSocket);
-		
 		copyFile(filename, message.getOut());
 		message.closeHttpRequest();
 	}
 
+	/**
+	 * Copies the requested file on the output stream.
+	 * 
+	 * @param filename
+	 *            The name of the requested file
+	 * @param out
+	 *            The OutputStreamWriter
+	 * @throws FileNotFoundException
+	 *             When the FileReader cannot find a file corresponding to
+	 *             filename
+	 * @throws IOException
+	 *             When something goes wrong while reading from file or writing
+	 *             to the OutputStream
+	 */
 	private void copyFile(String filename, OutputStreamWriter out)
 			throws FileNotFoundException, IOException {
+
 		BufferedReader fileReader = new BufferedReader(new FileReader(filename));
 		String fileLine = fileReader.readLine();
 		while (fileLine != null) {
 			out.write(fileLine + "\n");
 			fileLine = fileReader.readLine();
 		}
+
 	}
-	
+
 	/**
-	 * Controlla se il file è presente nella cartella del file,
-	 * altrimenti restituisce error.html
+	 * Controlla se il file è presente nella cartella del file, altrimenti
+	 * restituisce error.html
 	 * 
 	 * @param uri
 	 * @return
--- Laboratorio/HttpServer/src/server/HttpMessage.java
+++ Laboratorio/HttpServer/src/server/HttpMessage.java
@@ -18,7 +18,7 @@ public class HttpMessage {
 	public enum ContentType {
 		HTML("text/html"),
 		XML("text/xml");
-		
+
 		private String text;
 
 		private ContentType(String text) {
@@ -29,17 +29,27 @@ public class HttpMessage {
 	private ContentType contentType = ContentType.HTML;
 	private OutputStreamWriter out;
 
+	/**
+	 * Prints the header of the HTTP answer on the OutputStream defined by the
+	 * clientSocket. The contentType varies accordingly to the client's request
+	 * and avilability of the type in the enumeration
+	 * 
+	 * @param clientSocket
+	 *            the socket for the client
+	 * @throws IOException
+	 *             when unable to write on the OutputStream
+	 */
 	public void openHttpAnswer(Socket clientSocket) throws IOException {
 
 		this.out = new OutputStreamWriter(clientSocket.getOutputStream(),
 				Charset.forName("UTF-8").newEncoder());
 		out.write("HTTP/1.1 200 OK\n");
 		out.write("Date: Tue, 17 Mar 2014 14:47:00\n");
-		out.write("Content-Type: "+contentType.text+"; charset=utf-8\n");
+		out.write("Content-Type: " + contentType.text + "; charset=utf-8\n");
 		out.write("\n");
-		
+
 	}
-	
+
 	public void setContentType(ContentType contentType) {
 		this.contentType = contentType;
 	}
