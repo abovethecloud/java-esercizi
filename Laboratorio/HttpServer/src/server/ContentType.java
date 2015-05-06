190eb1394842c669533d65d10db68d8a004b9767
abovethecloud <claudio.peroni93@gmail.com>
2015-05-06 16:26:07 +0200

    HttpServer: Now added the enum as a separate file

---
+++ Laboratorio/HttpServer/src/server/ContentType.java
@@ -0,0 +1,22 @@
+package server;
+
+/**
+ * This enum contains all the possible content types for the HTTP response
+ * 
+ * @author claudio
+ *
+ */
+public enum ContentType {
+	HTML("text/html"),
+	XML("text/xml");
+
+	private String text;
+
+	private ContentType(String text) {
+		this.text = text;
+	}
+
+	public String getText() {
+		return text;
+	}
+}
